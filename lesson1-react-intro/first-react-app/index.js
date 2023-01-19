933951844af0a04cf894c1b1b64ab34ba77cf8d5
Uliana Koshuba <koshubaulyana@gmail.com>
2023-01-19 15:27:08 +0200

    commit

--- lesson1-react-intro/first-react-app/index.js
+++ lesson1-react-intro/first-react-app/index.js
@@ -1,11 +1,7 @@
 /* eslint-disable no-undef */
 
-const rootElement = document.querySelector("#root");
+const rootElement = document.querySelector('#root');
 
-const greetingElem = React.createElement(
-  "div",
-  { className: "greeting" },
-  "Hello, React!"
-);
+const greetingElem = React.createElement('div', { className: 'greeting' }, 'Hello, React!');
 
 ReactDOM.render(greetingElem, rootElement);
