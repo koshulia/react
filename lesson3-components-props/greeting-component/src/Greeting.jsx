953dfe1665c213269685d54bae8ae119e9a91b00
Uliana Koshuba <koshubaulyana@gmail.com>
2023-01-20 14:19:22 +0200

    update

--- lesson3-components-props/greeting-component/src/Greeting.jsx
+++ lesson3-components-props/greeting-component/src/Greeting.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import moment from 'moment';
 
-const fullAge = date => moment(date).fromNow();
-
+const fullAge = date => moment(date).fromNow(true);
 export default function Greeting(props) {
   return (
     <div className="greeting">
-      My name is {props.firstName} {props.lastName}. I am {fullAge(props.birthDate)} years old
+      My name is {props.firstName} {props.lastName}. I am {fullAge(props.birthDate)} old
     </div>
   );
 }
