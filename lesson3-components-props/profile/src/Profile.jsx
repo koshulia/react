b13128ad9d99976775732d97175e73ab89b0e0b7
Uliana Koshuba <koshubaulyana@gmail.com>
2023-01-22 20:03:27 +0200

    hw 2

---
+++ lesson3-components-props/profile/src/Profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import moment from 'moment';
+
+const formatDate = date => moment(date).format('DD MMM YYYY');
+
+export default function Profile(props) {
+  return (
+    <div className="profile">
+      <div className="profile__name">
+        {props.userData.firstName} {props.userData.lastName}
+      </div>
+      <div className="profile__birth">
+        Was born {formatDate(props.userData.birthDate)} in {props.userData.birthPlace}
+      </div>
+    </div>
+  );
+}
