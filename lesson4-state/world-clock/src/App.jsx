33daad44ae6ec7bbe166f05b46604f67ac1f6aa9
Uliana Koshuba <koshubaulyana@gmail.com>
2023-01-23 15:34:58 +0200

    hw 1

---
+++ lesson4-state/world-clock/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Clock from './Clock';
+
+const App = () => (
+  <>
+    <Clock location="London" offset={0} />
+    <Clock location="Kyiv" offset={2} />
+    <Clock location="New York" offset={-5} />
+  </>
+);
+
+export default App;
---
+++ lesson4-state/world-clock/src/Clock.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './clock.scss';
+import moment from 'moment';
+
+const formatTime = time => moment(time).format('hh:mm:ss A');
+
+const getTimeWithOffset = offset => {
+  const currentTime = new Date();
+  const utcOffset = currentTime.getTimezoneOffset() / 60;
+  return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
+};
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: props.location,
+      currentTime: getTimeWithOffset(props.offset),
+    };
+
+    // bad practise
+    setInterval(() => {
+      this.setState({
+        currentTime: moment(this.state.currentTime).add(1, 's'),
+      });
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <div className="clock">
+        <div className="clock__location">{this.state.location}</div>
+        <div className="clock__time">{formatTime(this.state.currentTime)}</div>
+      </div>
+    );
+  }
+}
+
+export default Clock;
