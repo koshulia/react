c9f8d966afe40b36a23305d4580db2c48e6cf858
Uliana Koshuba <koshubaulyana@gmail.com>
2023-01-28 17:20:46 +0200

    task 2

--- lesson5-events/good-button copy/src/GoodButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const GoodButton = () => (
-  <button className="fancy-button" onClick={() => alert('Good job!')}>
-    Click me!
-  </button>
-);
-
-export default GoodButton;
