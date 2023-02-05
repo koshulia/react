fb36b72838f80a43366e84ed71209bed10edb7a4
Uliana Koshuba <koshubaulyana@gmail.com>
2023-02-05 22:01:00 +0200

    update

--- lesson6-conditional-rendering/is-online/src/Online.jsx
+++ lesson6-conditional-rendering/is-online/src/Online.jsx
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 
 class Online extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.onOnline = props.onOnline;
-  // }
-
   render() {
     return <span className="status__text">All good!</span>;
   }
