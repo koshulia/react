67b661a18b945859e08746ec9a82868919dec135
Uliana Koshuba <koshubaulyana@gmail.com>
2023-02-09 17:28:19 +0200

    update

--- lesson8-lifecycle-methods/lifecycle-methods/src/Life.jsx
+++ lesson8-lifecycle-methods/lifecycle-methods/src/Life.jsx
@@ -10,11 +10,14 @@ class Life extends Component {
     console.log('componentDidMount: API calls, subscriptions');
   }
 
-  shouldComponentUpdate() {
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log(nextProps, nextState);
     console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
+    return nextProps.number % 2;
   }
 
-  componentDidUpdate() {
+  componentDidUpdate(prevProps, prevState) {
+    console.log(prevProps);
     console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
   }
 
--- lesson8-lifecycle-methods/lifecycle-methods/src/index.jsx
+++ lesson8-lifecycle-methods/lifecycle-methods/src/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Demo from './Demo';
+import Demo from './Demo';
 import './index.scss';
-import Life from './Life';
+// import Life from './Life';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<Life />, rootElement);
+ReactDOM.render(<Demo />, rootElement);
