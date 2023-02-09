467c5bcffe8f5e359cf518c871b8b8b7a0e4090f
Uliana Koshuba <koshubaulyana@gmail.com>
2023-02-09 18:34:42 +0200

    upd

--- lesson8-lifecycle-methods/lifecycle-methods/src/Life.jsx
+++ lesson8-lifecycle-methods/lifecycle-methods/src/Life.jsx
@@ -13,12 +13,13 @@ class Life extends Component {
   shouldComponentUpdate(nextProps, nextState) {
     console.log(nextProps, nextState);
     console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
-    return nextProps.number % 2;
+    return true;
   }
 
   componentDidUpdate(prevProps, prevState) {
-    console.log(prevProps);
+    console.log(prevProps, prevState);
     console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
+    return true;
   }
 
   componentWillUnmount() {
