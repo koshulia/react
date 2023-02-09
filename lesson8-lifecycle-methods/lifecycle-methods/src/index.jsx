c507c8c2adcfe4d966b769aebbb92c31df5b1ab4
Uliana Koshuba <koshubaulyana@gmail.com>
2023-02-09 17:17:58 +0200

    task 1

---
+++ lesson8-lifecycle-methods/lifecycle-methods/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Demo from './Demo';
+import './index.scss';
+// import Life from './Life';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<Demo />, rootElement);
