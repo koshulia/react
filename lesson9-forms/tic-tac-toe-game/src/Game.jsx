751bfbd16231b2bf66d940433bd54ddfe686123a
Uliana Koshuba <koshubaulyana@gmail.com>
2023-02-18 20:26:19 +0200

    update

--- lesson9-forms/tic-tac-toe-game/src/Game.jsx
+++ lesson9-forms/tic-tac-toe-game/src/Game.jsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import Board from './Board';
 
+const calculateWinner = squares => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i += 1) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+};
+
 class Game extends React.Component {
   state = {
     history: [
@@ -16,7 +37,7 @@ class Game extends React.Component {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
-    if (this.props.calculateWinner(squares) || squares[i]) {
+    if (calculateWinner(squares) || squares[i]) {
       return;
     }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
@@ -41,7 +62,7 @@ class Game extends React.Component {
   render() {
     const { history } = this.state;
     const current = history[this.state.stepNumber];
-    const winner = this.props.calculateWinner(current.squares);
+    const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
       const desc = move ? `Go to move # ${move}` : 'Go to game start';
--- lesson9-forms/tic-tac-toe-game/src/index.jsx
+++ lesson9-forms/tic-tac-toe-game/src/index.jsx
@@ -3,26 +3,5 @@ import ReactDOM from 'react-dom';
 import Game from './Game';
 import './index.scss';
 
-const calculateWinner = squares => {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  for (let i = 0; i < lines.length; i += 1) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-};
-
 const root = document.querySelector('#root');
-ReactDOM.render(<Game calculateWinner={calculateWinner} />, root);
+ReactDOM.render(<Game />, root);
