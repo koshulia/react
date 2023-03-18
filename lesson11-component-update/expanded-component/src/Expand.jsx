7d9bf2cd6f3d75fe416bcd07368d931e59cf1eac
Uliana Koshuba <koshubaulyana@gmail.com>
2023-03-19 00:02:53 +0200

    update

--- lesson11-component-update/expanded-component/src/Expand.jsx
+++ lesson11-component-update/expanded-component/src/Expand.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 class Expand extends Component {
   state = {
@@ -19,14 +19,20 @@ class Expand extends Component {
       <div className="expand border">
         <div className="expand__header">
           <span className="expand__title">{this.props.title}</span>
-
           <button className="expand__toggle-btn" onClick={this.toggleExpansion}>
             {this.state.isShown ? (
-              <i className="fa-solid fa-chevron-up"></i>
+              <FontAwesomeIcon icon={solid('chevron-up')} />
+            ) : (
+              <FontAwesomeIcon icon={solid('chevron-down')} />
+            )}
+          </button>
+          {/* <button className="expand__toggle-btn" onClick={this.toggleExpansion}>
+            {this.state.isShown ? (
+               <i className="fa-solid fa-chevron-up"></i>
             ) : (
               <i className="fa-solid fa-chevron-down"></i>
             )}
-          </button>
+          </button> */}
         </div>
         {this.state.isShown ? <div className="expand__content">{this.props.children}</div> : null}
       </div>
