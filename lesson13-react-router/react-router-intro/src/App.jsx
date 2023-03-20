d7b762816c9be5c1a94c420860d860fe05f141eb
Uliana Koshuba <koshubaulyana@gmail.com>
2023-03-21 01:42:57 +0200

    task 1

---
+++ lesson13-react-router/react-router-intro/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Contacts from './Contacts';
+import Home from './Home';
+import PageNotFound from './PageNotFound';
+import Products from './Products';
+
+const App = () => (
+  <div className="page">
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route path="/products">
+          <Products />
+        </Route>
+
+        <Route path="/contacts">
+          <Contacts />
+        </Route>
+
+        <Route path="*">
+          <PageNotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  </div>
+);
+
+export default App;
